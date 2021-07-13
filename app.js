38710118b88b993d0b17a2c783c83ca3267b769e
bbhupen <bbhupen601@gmail.com>
2021-07-13 15:53:09 +0530

    Index updated (Removed read only one string, now it'l read a sentence)

--- app.js
+++ app.js
@@ -7,20 +7,31 @@ token = process.env.TOKEN;
 const prefix = "/";
 
 
+function reply(message) { 
+  if (message == "hello"){
+    message.channel.send("Tumi leu neki"); 
+  }
+  
+}
+
+
+let mapMsgs = new Map();
+mapMsgs.set("hello", reply);
+
+
 client.on("message", function(message) {
 
-    let commands = ["ping", "hello", "kela", "baal", "oi_xet", "bhat_khabi_neki", "Kwanzo_gay_neki", "oi_kwanzou", "test"]
+  let commands = ["ping", "hello", "kela", "baal", "oi xet", "bhat khabi neki", "kwanzo gay neki", "oi kwanzou", "test"]
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
   
   const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
+  const command = commandBody.toLowerCase();
+  console.log(command)
 
     if (commands.includes(command)){
-        console.log("HU")
-        message.reply("Exists")
+        mapMsgs.get(command)(message)
     }
     else {
         message.reply("Not a valid command")
